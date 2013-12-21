e8b5a1fc2ccc76a3e4c357e766958f1fc751428a
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-12-21 12:55:45 -0600

    add Unicode::Number get_number_system_by_name method

--- lib/Unicode/Number.pm
+++ lib/Unicode/Number.pm
@@ -5,12 +5,24 @@ use strict;
 use warnings;
 
 use Alien::Uninum;
+use List::AllUtils qw/first/;
 
 use XSLoader;
 XSLoader::load( 'Unicode::Number', $Unicode::Number::VERSION );
 
 sub new {
 	bless {}, shift;
+}
+
+=method get_number_system_by_name($name)
+
+Returns the L<Unicode::Number::System> that has the name given by the $name
+parameter (string) or C<undef> if not found.
+
+=cut
+sub get_number_system_by_name {
+	my ($self, $name) = @_;
+	return first { $_->name eq $name } @{ $self->list_number_systems };
 }
 
 1;
