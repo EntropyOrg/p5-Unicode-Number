951f945a1d6cec53d4cb5fad83531d34eb1bb8dc
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-12-21 21:25:54 -0600

    Build results of 71d7736 (on master)

--- lib/Unicode/Number.pm
+++ lib/Unicode/Number.pm
@@ -35,6 +35,7 @@ sub string_to_number {
 	}
 	croak "Invalid number system\n" unless defined $ns_id;
 	my $digits_string_u32 = encode('UTF-32', $digits_string);
+	use DDP; p $digits_string_u32;
 	my $str = $self->_StringToNumberString($digits_string_u32, $ns_id);
 	use DDP; p $str;
 	return $str;
