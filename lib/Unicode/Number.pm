6a1978e82b4dcc8124aea14245bf9ded7b7a1137
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-12-24 09:14:13 -0600

    Build results of 70a3175 (on master)

--- lib/Unicode/Number.pm
+++ lib/Unicode/Number.pm
@@ -1,6 +1,6 @@
 package Unicode::Number;
 {
-  $Unicode::Number::VERSION = '0.002';
+  $Unicode::Number::VERSION = '0.003';
 }
 
 use strict;
@@ -108,7 +108,7 @@ Unicode::Number - handle numerals in Unicode using the libuninum library
 
 =head1 VERSION
 
-version 0.002
+version 0.003
 
 =head1 SYNOPSIS
 
