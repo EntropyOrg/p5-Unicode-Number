40ff1c45d031de7867f86a7161514a4c88a4e662
Zakariyya Mughal <zaki.mughal@gmail.com>
2013-12-23 18:57:19 -0600

    Build results of 77ae7f7 (on master)

--- inc/UninumMakeMaker.pm
+++ inc/UninumMakeMaker.pm
@@ -7,8 +7,8 @@ extends 'Dist::Zilla::Plugin::MakeMaker::Awesome';
 
 override _build_WriteMakefile_args => sub { +{
     %{ super() },
-    CCFLAGS => "Alien::Uninum->new->cflags",
-    LIBS => [ "Alien::Uninum->new->cflags" ],
+    CCFLAGS => \ sub { require Alien::Uninum; Alien::Uninum->new->cflags },
+    LIBS => [ \ sub { require Alien::Uninum; Alien::Uninum->new->cflags } ],
     XS      => {
         "lib/Unicode/Number.xs" => "lib/Unicode/Number.c",
     }
